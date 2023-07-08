c42823385aea8ef12dc784609f7ab9bcc8f13fe7
g1alexander <alexander.granados.dev@gmail.com>
2023-07-07 21:32:27 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by g1alexander on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
