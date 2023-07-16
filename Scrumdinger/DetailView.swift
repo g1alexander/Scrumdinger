84a525d89fbf706d5e7f8122ec6939a3bf75d6a8
g1alexander <alexander.granados.dev@gmail.com>
2023-07-16 10:18:39 -0500

    feat: create view DetailScrum and navigation Scrum views

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,51 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by g1alexander on 7/16/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List{
+            Section( header: Text(scrum.title) ){
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                
+                HStack{
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack{
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleDate[0]
+    
+    static var previews: some View {
+        NavigationStack{
+            DetailView(scrum: scrum)
+        }
+    }
+}
