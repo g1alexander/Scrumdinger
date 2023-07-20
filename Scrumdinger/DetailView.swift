403d2ea28c9239af756299b7b3fa7f97d720371f
g1alexander <alexander.granados.dev@gmail.com>
2023-07-20 11:52:50 -0500

    feat: create view edit scrum and create state

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -11,6 +11,8 @@ struct DetailView: View {
     
     let scrum: DailyScrum
     
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List{
             Section( header: Text(scrum.title) ){
@@ -46,6 +48,31 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar{
+            Button(action: { isPresentingEditView = true }) {
+                Text("Edit")
+            }
+            .accessibilityLabel("Edit Scrum")
+        }
+        .sheet(isPresented: $isPresentingEditView){
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction){
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction){
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+            
+        }
     }
 }
 
